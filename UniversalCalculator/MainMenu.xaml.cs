c77701ec3b444a29a89fb6beb48334c8527e3a87
001121334 <thi.mai12@student.tafesa.edu.au>
2024-09-07 13:03:18 +0930

    Add new MainMenu page with logo and update app xaml to display MainMenu first.

---
+++ UniversalCalculator/MainMenu.xaml.cs
@@ -0,0 +1,54 @@
+﻿using Windows.ApplicationModel.Core;
+using Windows.Foundation;
+using Windows.UI.Core;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Calculator
+{
+	public sealed partial class MainMenu : Page
+	{
+		// default var
+		public static MainMenu mainmenu { get; set; }
+
+		// default functions
+		public MainMenu()
+		{
+			InitializeComponent();
+
+			mainmenu = this;
+
+		}
+
+		// page functions
+		private void pageLoaded(object sender, RoutedEventArgs e)
+		{
+			// window minimum size
+			ApplicationView.GetForCurrentView().SetPreferredMinSize(new Size(256, 384));
+
+			// enable title bar full customiztion
+			CoreApplication.GetCurrentView().TitleBar.ExtendViewIntoTitleBar = true;
+			// title bar customization
+			ApplicationViewTitleBar titleBar = ApplicationView.GetForCurrentView().TitleBar;
+
+			titleBar.ButtonBackgroundColor = Windows.UI.Colors.Transparent;
+			titleBar.ButtonInactiveBackgroundColor = Windows.UI.Colors.Transparent;
+			titleBar.ButtonInactiveForegroundColor = Windows.UI.Colors.White;
+		}
+
+		private void MathsCalculator_Click(object sender, RoutedEventArgs e)
+		{
+		}
+		private void MortgageCalculator_Click(object sender, RoutedEventArgs e)
+		{
+		}
+		private void CurrencyConverter_Click(object sender, RoutedEventArgs e)
+		{
+		}
+
+		private void Exit_Click(object sender, RoutedEventArgs e)
+		{
+		}
+	}
+}
